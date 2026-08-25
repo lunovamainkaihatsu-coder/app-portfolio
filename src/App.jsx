da50de690d7b987838032201e9ae26644c075ba3
lunovamainkaihatsu-coder <lunova.main.kaihatsu@gmail.com>
2026-08-26 07:29:07 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3081,6 +3081,19 @@ const APPS = [
     // 今日の日付などに更新
     updatedAt: "2026-8-25",
   },
+      {
+    id: "app-238",
+    name: " 今日の1個",
+    tagline: " 今日の1個",
+    description: "  今日の1個",
+    // ↓ここを、ご主人のアプリのURLに差し替えてね
+    url: "https://mainitiapp-kzcvxhl56sg88fkytatfro.streamlit.app/",
+    repo: "",
+    tags: ["Web"],
+    status: "公開中",
+    // 今日の日付などに更新
+    updatedAt: "2026-8-26",
+  },
 ];
 
 
@@ -5214,6 +5227,15 @@ const POSTS = [
     date: "2026-8-25", 
     // ↓ここを記事のURLに変える
     url: "https://note.com/lunova_create/n/n1334af901f13?app_launch=false",
+    tags: ["DevLog"],
+  },
+      {
+    id: "note-238",
+    title: "今日の1個を公開しました",
+    // 公開した日
+    date: "2026-8-26", 
+    // ↓ここを記事のURLに変える
+    url: "https://note.com/lunova_create/n/ndd541229a570",
     tags: ["DevLog"],
   },
 ];
